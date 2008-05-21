86d184b317164b7f5f20265256b86bcb22578b85
goran.stack <goran.stack@8cb19435-aa4d-0410-9f13-addb4186d417>
2008-05-21 12:23:25 +0000

    Initial import.

---
+++ MemoryMonitor/src/org/apache/batik/util/gui/TestMemoryMonitor.java
@@ -0,0 +1,11 @@
+package org.apache.batik.util.gui;
+
+import org.apache.batik.util.gui.MemoryMonitor;
+
+public class TestMemoryMonitor {
+
+	public static void main(String[] args) {
+		MemoryMonitor memoryMonitorFrame = new MemoryMonitor();
+		memoryMonitorFrame.setVisible(true);
+	}
+}
