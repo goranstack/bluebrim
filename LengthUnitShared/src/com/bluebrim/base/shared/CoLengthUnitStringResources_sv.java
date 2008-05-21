40bb7b47f21deed8e3431d613dcb2c3ba7453a92
goran.stack <goran.stack@8cb19435-aa4d-0410-9f13-addb4186d417>
2008-05-21 12:22:02 +0000

    Initial import.

---
+++ LengthUnitShared/src/com/bluebrim/base/shared/CoLengthUnitStringResources_sv.java
@@ -0,0 +1,15 @@
+package com.bluebrim.base.shared;
+
+public class CoLengthUnitStringResources_sv extends CoLengthUnitStringResources
+{
+	static final Object[][] contents =
+	{
+		{ "POINTS", "" },
+		{ "MM", "mm" },
+		{ "CM", "cm" },
+		{ "INCH", "tum" },
+	};
+public Object[][] getContents ( ) {
+	return contents;
+}
+}
